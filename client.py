5adc42fe1d535b381d56a38ee7ea3884a2e2734e
Chaws-Jewn <150521807+Chaws-Jewn@users.noreply.github.com>
2023-11-15 05:32:20 +0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,102 @@
+import socket, threading
+import tkinter as tk
+from tkinter import scrolledtext, messagebox
+
+root = tk.Tk()
+root.geometry("700x800")
+root.resizable(0,0)
+
+DARK_OCEAN = "#1A5276"
+OCEAN = "#1F618D"
+DARK_BLUE = "#154360"
+
+BTN_FONT = ""
+LABEL_FONT = ("Amasis MT Pro Black", 25)
+MSG_FONT = ("Arial", 20)
+
+# Send message, reset entry
+def send_message():
+    message = message_entry.get()
+    client.sendall(message.encode("utf-8"))
+    message_entry.delete(0, len(message))
+
+# Send message keybind <Return>
+def send_message_key(event):
+    send_message()
+
+# Infinite loop for receiving messages from server
+def receive_messages():
+    while True:
+        message = client.recv(1024).decode("utf-8")
+        message_area.config(state=tk.NORMAL)
+        message_area.insert(tk.END, message + '\n')
+        message_area.config(state=tk.DISABLED)
+
+# Create username before joining server
+def create_username():
+    username_entry.config(state= "disabled")
+    join_chat.config(state= "disabled")
+    connect_to_server(username_entry.get())
+
+# Error on getting username, allow resubmission of username
+def enter_new_username():
+    username_entry.config(state= tk.NORMAL)
+    username_entry.delete(0, len(username_entry.get()))
+    join_chat.config(state= tk.NORMAL)
+
+# Server connection
+def connect_to_server(username):
+    global client
+    server_address = ("127.0.0.1", 5555)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client.connect(server_address)
+        check_username(username)
+    except Exception as e:
+        print(f"Error: {e}")
+
+# Check matching username on server
+def check_username(username):
+    try:
+        client.sendall(username.encode("utf-8"))
+        server_response = client.recv(1024).decode("utf-8")
+        if server_response == "success":
+            # After success on sending username
+            message = f"{client.recv(1024).decode("utf-8")}"
+            message_area.config(state=tk.NORMAL)
+            message_area.insert(tk.END, message + '\n')
+            message_area.config(state=tk.DISABLED)
+            message_entry.config(state=tk.NORMAL)
+            send_message_btn.config(state=tk.NORMAL)
+            message_entry.focus_set()
+            root.bind("<Return>", func= send_message_key)
+            threading.Thread(target=receive_messages).start()
+            threading.Thread(target=send_message).start()
+        
+        elif server_response == "failed":
+            warning = messagebox.showwarning(title= "Username warning", message= "Username has already been used. Please choose another.")
+            enter_new_username()
+        
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+# UI configuration
+root.configure(background= DARK_BLUE)
+title = tk.Label(text= "Chat Room", font= LABEL_FONT, width= 10, foreground= "white", background= DARK_BLUE).grid(row=0, column=0, columnspan= 3, pady= (10, 0))
+username_box = tk.Label(text= "Username: ", font= MSG_FONT, width= 10, foreground= "white", background= OCEAN).grid(row=1, column=0, padx= (10, 0), pady= 3)
+username_entry = tk.Entry(justify= "right", font= MSG_FONT, width= 25, foreground= "white", background= OCEAN, border= 5)
+username_entry.grid(row=1, column=1, pady= 3)
+join_chat = tk.Button(text= "Join", font= ("Arial", 15), command= create_username, foreground= "white", background= OCEAN, border= 5)
+join_chat.grid(row= 1, column= 2, padx= (0, 15))
+
+message_area = scrolledtext.ScrolledText(root, font= MSG_FONT, bg= OCEAN, fg= "white", width=43, height= 18, border= 5)
+message_area.grid(row=2, column=0, columnspan= 3, padx= 10, pady= 20)
+message_area.config(state=tk.DISABLED)
+
+message_entry = tk.Entry(state= tk.DISABLED, justify= "right", font= MSG_FONT, width= 35, foreground= "white", background= OCEAN, border= 5)
+message_entry.grid(row=3, column=0, columnspan= 2, pady= (10,0))
+send_message_btn = tk.Button(state=tk.DISABLED, text= "Send", font= ("Arial", 15), command= send_message, foreground= "white", background= OCEAN, border= 5)
+send_message_btn.grid(row= 3, column= 2, pady= (10, 0), padx= (0, 15))
+
+root.mainloop()
---
+++ server.py
@@ -0,0 +1,67 @@
+import socket, threading
+
+clients = []
+
+def start_server():
+    server_address = ("127.0.0.1", 5555)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(server_address)
+    print(f"Server started on {server_address[0]}:{server_address[1]}")
+
+    while True:
+        accept_client(server)
+
+def accept_client(server):
+    server.listen(20)
+
+    while True:
+        client_socket, client_addr = server.accept()
+        username = client_socket.recv(1024).decode("utf-8").strip()
+        stop = False
+        response = b"success"
+        if len(clients) < 1:
+            client_socket.sendall(response)
+            stop = True
+
+        # Check matching client usernames
+        else:
+            for x in clients:
+                if x[0] == username:
+                    response = (b"failed")
+
+            client_socket.sendall(response)
+            if response == b"success":
+                stop = True
+
+        # Collect user information
+        if stop:
+            clients.append([username, client_socket])
+            send_message_to_clients(f"{clients[len(clients)-1][0]} connected to the server")
+            print(f"Client {clients[len(clients)-1][0]} connected successfully at {client_addr[0]}:{client_addr[1]}")
+            threading.Thread(target=receive_message, args=(client_socket, )).start()
+            break
+            
+        # Close socket to allow reconnection
+        else:
+            client_socket.close()
+
+def receive_message(client):
+    while True:
+        # Find the username of the client (sender)
+        user = ""
+        for i in range(len(clients)):
+            if clients[i][1] == client:
+                user = clients[i][0]
+                break
+        message = f"[{user}]: {client.recv(1024).decode("utf-8")}"
+
+        # Send message to clients if a message is received
+        send_message_to_clients(message)
+        print(message)
+
+def send_message_to_clients(message):
+    # Send message to all clients
+    for i in clients:
+        i[1].sendall(message.encode("utf-8"))
+
+start_server()
